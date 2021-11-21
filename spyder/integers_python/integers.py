58652fdb4b2618c4265022e2fed8b8f99b8fa3d2
ET <etyy1999@gmail.com>
2021-11-21 16:30:17 +0800

    printing 1 to 10

---
+++ spyder/integers_python/integers.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 21 15:50:20 2021
+
+@author: Evangeline Tan
+"""
+
+#%%
+print(1,2,3,4,5,6,7,8,9,10)
+
+
+#%%
+for i in range(1,11):
+    print(i)
+print("that's all")
